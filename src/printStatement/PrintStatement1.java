824d80e20a1a7132c2dbea78c5c05775d0e92a84
Sonu1009u <sonusirjjisss@gmail.com>
2022-10-30 15:57:45 +0530

    Java Print Statement

---
+++ src/printStatement/PrintStatement1.java
@@ -0,0 +1,30 @@
+package printStatement;
+
+public class PrintStatement1 {
+    // Note : We are following W3 tutorials of Java
+    public static void main(String[] args) {
+        System.out.println("November Java Project Starts from Here.");
+        System.out.println("Hello Ravindra Ji.");
+        System.out.println("My name is sonu Kumar.");
+        System.out.println("I proud to be an Indian.");
+        System.out.println("I am trying to learn java.");
+        // print with only print
+        System.out.println("This line is print with only print....");
+        System.out.println("this is Second line with print....");
+        // if we will not use println() than it will print all line in one row.
+
+        // now we will use integer values
+        System.out.println(12);
+        System.out.println(158);
+        System.out.println(125);
+        System.out.println(156);
+
+        // we can perform calculator method in sop
+        System.out.println("We are doing mathematics question here :");
+        System.out.println(2*5+12-6+75);
+        System.out.println(78-48+35*285-65);
+
+        // if you want to make our code as a comment than select of code lines and use ctrl+*+/
+
+    }
+}
