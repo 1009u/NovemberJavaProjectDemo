2dabc20b4272c7418ef34dd7076a9ede24e623ca
Sonu1009u <sonusirjjisss@gmail.com>
2022-10-30 16:15:48 +0530

    Add Types of Variable

--- src/variables/VariableDemo1.java
+++ src/variables/VariableDemo1.java
@@ -8,5 +8,23 @@ public class VariableDemo1 {
         //float - stores floating point numbers, with decimals, such as 19.99 or -19.99
         //char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
         //boolean - stores values with two states: true or false
+        String name="Taj Mahal";
+        System.out.println("The best indian Historical Place :"+name);
+
+        int year=1648;
+        System.out.println("Taj make year :"+year);
+
+        float area= 10000.45f;
+        System.out.println("Average are of Taj : "+area);
+
+        //Note when we use char on than time we will not use double cote
+        char word_of_Taj='T';
+        System.out.println("Taj Mahal Start word with :"+word_of_Taj);
+
+        boolean color=true;
+        System.out.println("Color of Taj is White :"+color);
+
+
+
     }
 }
